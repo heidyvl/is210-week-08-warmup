7cd15bdc3cd982091acdc145d0636c0fc2dba5f2
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-03-17 01:15:22 +0000

    task_02

---
+++ task_02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A docstring."""
+
+
+def bool_to_str(bval):
+    """True or False"""
+    if bval is True:
+        return 'Yes'
+    else:
+        return 'No'
